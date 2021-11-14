afd632655ef00dea68156cf23861ab82f2b9a930
Roman <mister02.rv@gmail.com>
2021-11-15 00:02:28 +0200

    remove comments

--- handler.go
+++ handler.go
@@ -5,8 +5,6 @@ import (
 	"io"
 )
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
 	Input  io.Reader
 	Output io.Writer
